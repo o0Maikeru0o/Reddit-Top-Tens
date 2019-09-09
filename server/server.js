c1950ae3d2849510113b61ed7cd3fa5be03e3b0f
o0Maikeru0o <mnickandros@gmail.com>
2019-09-09 19:43:10 -0400

    wrote user auth api req

--- server/server.js
+++ server/server.js
@@ -13,3 +13,31 @@ app.use(parser.urlencoded({ extended: true }));
 app.use('/', express.static(`${__dirname}/../public`));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
+app.get('/top10', (req, res) => {
+  axios({
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'post',
+    url: access,
+    auth: {
+      username: clientId,
+      password: secret,
+    },
+  })
+    .then((result) => result.data.access_token)
+    // .then((token) => {
+    //   console.log(token);
+    //   axios({
+    //     url: 'https://oauth.reddit.com/subreddits/mine/subscriber',
+    //     method: 'get',
+    //     headers: {
+    //       Authorization: `Bearer ${token}`,
+    //     },
+    //   });
+    // })
+    // .then((result) => console.log(result))
+    .catch((err) => console.log(err));
+});
